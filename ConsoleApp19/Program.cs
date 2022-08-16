2e2b7dbe1e2b238277190099b4136f37b9a8f938
yusbaklee <67943334+yusbaklee@users.noreply.github.com>
2022-08-16 08:58:37 +0100

    Add project files.

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assingment7EventAndDelegates
+{
+
+    internal class Delegates1
+    {
+        public delegate bool Predicate(XClass x);
+
+        void LambdaExpansion()
+        {
+
+            var likelyUsername = "Yomi";
+
+            Predicate classwork = (XClass x) =>
+            {
+                if (x.UserName == likelyUsername)  //check if Username is equal to LikelyUsername
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            };
+        }
+    }
+
+
+
+    class XClass
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+
+        public string UserName { get; set; }
+
+    }
+}
